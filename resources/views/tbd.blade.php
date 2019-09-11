633b7a8baed92484db17de325d4bf86b7b321c26
Lucky.JC <lucky.jonathan.lc@gmail.com>
2019-09-11 17:06:39 +0700

    under construction

---
+++ resources/views/tbd.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Dota 2 but... Tubes AI</title>
+	<link href="/css/style.css" rel="stylesheet" type="text/css"/>
+	<link rel="shortcut icon" href="/img/favicon.ico">
+</head>
+<body>
+	<div class="tbd-container">
+		<center><h2> LOOK FORWARD TO OUR &nbsp;&nbsp;&nbsp;<b>TUBES AI</b></h2></center>
+		<hr class="tbd-line">
+		<ul class="tbd-list">
+			<li>13517070 - Aidil Rezjki Suljztan S.</li>
+			<li>13517109 - Fata Nugraha</li>
+			<li>13517115 - Edward Alexander Jaya</li>
+			<li>13517136 - Lucky Jonathan Chandra</li>
+		</ul>
+	</div>
+</body>
+</html>
